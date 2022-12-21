04d1af7ac06b1f62cd86164a714900b807bc67cb
Lucas David Erkana <lderkana21x@gmail.com>
2022-12-21 11:11:02 +0200

    Construct Book Class and export(ES6 method)

---
+++ modules/constructor.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+export default class Book {
+    constructor(title, author) {
+      this.id = new Date().valueOf();
+      this.title = title;
+      this.author = author;
+    }
+  }
